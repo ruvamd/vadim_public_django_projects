0bc74f00bd3deb4a0750bb552cba77cb05eb0cfc
vadim rusu <ruvamd@gmail.com>
2021-12-21 21:45:48 -0800

    .

--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -20,4 +20,7 @@ class Entry(models.Model):
  
     def __str__(self):
         """Return a string representation of the model."""
-        return f"{self.text[:50]}..."
+        if self.text[:]<=self.text[:50]:
+            return f"{self.text[:50]}"
+        else:
+            return f"{self.text[:50]}..."
