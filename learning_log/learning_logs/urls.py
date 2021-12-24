7365fbd0865f1d37e91fc5f28b07c4256e9368b2
vadim rusu <ruvamd@gmail.com>
2021-12-24 11:56:33 -0800

    .

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -11,5 +11,7 @@ urlpatterns = [
     #page that shows all topics
     path('topics/',views.topics,name='topics'),
     #detail path for a single topic
-    path('topics/<int:topic_id/',views.topic,name='topic')
+    path('topics/<int:topic_id/',views.topic,name='topic'),
+    # Page for adding a new topic
+    path('new_topic/', views.new_topic, name='new_topic'),
 ]
