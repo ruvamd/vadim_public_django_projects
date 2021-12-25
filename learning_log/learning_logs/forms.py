8680616678dc44946d4d138b08ea7d5dce989ba6
vadim rusu <ruvamd@gmail.com>
2021-12-25 02:43:26 -0800

    .

--- learning_log/learning_logs/forms.py
+++ learning_log/learning_logs/forms.py
@@ -8,3 +8,10 @@ class TopicForm(forms.ModelForm):
         fields = ['text']
         labels = {'text': ''}
         widgets = {'text': forms.Textarea(attrs={'cols': 80})}
+
+class EntryForm(forms.ModelForm):
+    class Meta:
+        model = Entry
+        fields = ['text']
+        labels = {'text': 'Entry:'}
+        widgets = {'text': forms.Textarea(attrs={'cols': 80})}
--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -14,4 +14,6 @@ urlpatterns = [
     path('topics/<int:topic_id/',views.topic,name='topic'),
     # Page for adding a new topic
     path('new_topic/', views.new_topic, name='new_topic'),
+    # Page for editing an entry.
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
 ]
