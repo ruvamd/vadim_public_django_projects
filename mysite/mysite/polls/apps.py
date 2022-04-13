53f275fc1ca630d4429e4c4728d5a1fba19ee92c
vadim rusu <ruvamd@gmail.com>
2022-04-13 04:26:19 -0700

    mysite project setup

--- mysite/mysite/polls/apps.py
+++ mysite/mysite/polls/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class LearningLogsConfig(AppConfig):
+class PollsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'learning_logs'
+    name = 'polls'
